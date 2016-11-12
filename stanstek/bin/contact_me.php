2ca60c8a68c58023cbc4ff297a5e19f76c9d3277
armanraftari <Raftari1@hotmail.com>
2016-11-12 18:08:58 +0100

    Update contact_me.php

--- stanstek/bin/contact_me.php
+++ stanstek/bin/contact_me.php
@@ -16,7 +16,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'raftari1@hotmail.com'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'Raftari1@hotmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "meddelande från Stanstek.se:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "Du har fått ett meddelande/fråga från Stanstek.se\n\n"."Här är detaljerna :\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@Stanstek.se\n";
